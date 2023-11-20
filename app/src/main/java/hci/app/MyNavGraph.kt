1ec7ba880d0aa03ad4dcb94feced035fb7d7e498
Tim S <84805291+tm-sm@users.noreply.github.com>
2023-11-20 16:37:37 -0300

    a

--- app/src/main/java/hci/app/MyNavGraph.kt
+++ app/src/main/java/hci/app/MyNavGraph.kt
@@ -1,25 +1,27 @@
 package hci.app
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import hci.app.ui.main.MainViewModel
 
 @Composable
-fun MyNavGraph(navController: NavHostController) {
+fun MyNavGraph(navController: NavHostController, viewModel: MainViewModel) {
     NavHost(
         navController = navController,
         startDestination = Screen.HomeScreen.route,
 
     ) {
         composable(Screen.HomeScreen.route) {
-            HomeScreen()
+            HomeScreen(viewModel = viewModel)
         }
         composable(Screen.RoutinesScreen.route) {
-            RoutinesScreen()
+            RoutinesScreen(viewModel = viewModel)
         }
         composable(Screen.SettingsScreen.route) {
-            SettingsScreen()
+            SettingsScreen(viewModel = viewModel)
         }
     }
 }
--- app/src/main/java/hci/app/SettingsScreen.kt
+++ app/src/main/java/hci/app/SettingsScreen.kt
@@ -7,9 +7,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHostController
+import hci.app.ui.main.MainUiState
+import hci.app.ui.main.MainViewModel
 
 @Composable
-fun SettingsScreen() {
+fun SettingsScreen(viewModel: MainViewModel) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
